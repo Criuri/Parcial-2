783754f542c079e1708f52637c5e8d6b078bcf7f
Cristian Uribe <uribecristianb@gmail.com>
2025-04-04 16:01:39 -0500

    Creacion de clases

---
+++ Parcial 2/Clases/clsUpload.cs
@@ -0,0 +1,13 @@
+﻿using Parcial_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parcial_2.Clases
+{
+	public class clsUpload
+	{
+
+    }
+}
